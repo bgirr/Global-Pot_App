202034dd0cbc71567533a24ec3426ecb883ba8b5
bgirr <girr.benjamin@gmail.com>
2017-01-21 10:51:28 +0100

    Insert return values

--- Recipes_API.js
+++ Recipes_API.js
@@ -1,51 +1,71 @@
 var Observable = require("FuseJS/Observable");
+var Storage = require("FuseJS/Storage");
 var recipes = Observable();
 var recipe_number = Observable();
 var data = Observable();
 var recipe_URL = Observable();
 var URL = Observable();
-var recipe_id = Observable();
 var recipe_number = Observable();
 var Zutat = Observable();
+var ingredient = Observable();
+var SAVENAME = "localStorage.json";
+var data = Observable();
 
 recipe_URL.value = 'https://cookingtest-cookingtest.rhcloud.com/recipes/api/recipes/';
 
+function createURL(BasicURL, recipeID){
+	var myURL = BasicURL + recipeID;
+	return myURL;
+}
+			
+
+fetch("http://cookingtest-cookingtest.rhcloud.com/recipes/api/recipes/")
+	.then(function(result) {
+	result.json().then(function(data) {
+	debug_log("Übersicht ist da!");
+
+	for (var i=0; i< 30; i++) {
+	var item = data[i];
+	recipes.add(item);
+		}
+	});
+});
 
 
-			fetch("http://cookingtest-cookingtest.rhcloud.com/recipes/api/recipes/")
-				.then(function(result) {
-					result.json().then(function(data) {
-						debug_log("Übersicht ist da!");
+function getRecipe(recipeURL) {	
+	debug_log(recipeURL);
+	var myrecipe;
 
-						for (var i=0; i< 30; i++) {
-							var item = data[i];
-							recipes.add(item);
+	fetch(recipeURL)
+	.then(function(result) {
+		result.json().then(function(data) {
+			debug_log("Rezept geladen!");
+		});
+	});
 
-						}
-					});
-				});
-
-	
-	URL.value = recipe_URL.value + recipe_number.value;
-	debug_log(URL.value);
-    fetch(URL.value)
+    fetch(recipeURL)
     	.then(function(response) { return response.json(); })
-        .then(function(responseObject) { data.value = responseObject;});
+        .then(function(responseObject) {myrecipe.value = responseObject;});
         debug_log('Richtiges JS!');	
-        
+        console.log(JSON.stringify(myrecipe));
+        //Storage.write(SAVENAME, JSON.stringify(data));
 
-    fetch(URL.value)
+    
+
+    fetch(recipeURL)
     	.then(function(result){
     		result.json().then(function(rezept){
     			debug_log("Neues Rezept geladen!");
 
     		for (var i=0; i<30; i++) {
     			var Zutaten = rezept[i];
-    			Zutat.add(item);
+    			ingredient.add(item);
     			}
     		});
     	});
-
+    debug_log(ingredient.value);
+    return myrecipe;
+}
 
 function overview_clicked(){
 
@@ -59,10 +79,13 @@ function overview_clicked(){
 
 function recipe_clicked(a){
 		//debug_log(arg);
-		console.log(JSON.stringify(a));
-		debug_log(a.data.id);
+		//console.log(JSON.stringify(a));
+		URL.value = createURL(recipe_URL.value, a.data.id);
+		data.value = getRecipe(URL.value);
+		console.log(JSON.stringify(data));
 		router.goto('RecipePage');
 
+		return data;
 			}
 
 				
